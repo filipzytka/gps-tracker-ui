55d24bf1c13090de95752232934f6f9f6416a12e
filipzytka <filip.zytka@gmail.com>
2025-01-10 15:44:43 +0100

    move hero to landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import Header from "./components/header/header";
-import Hero from "./components/hero/hero";
 import Footer from "./components/footer/footer";
 import CustomAccordion from "./components/accordion/accordion";
 import { accordionData, features } from "./data";
@@ -18,6 +17,46 @@ export default  function Home() {
       <Footer/>
     </div>
   );
+}
+
+const Hero = () => {
+  return (
+    <div className="bg-white">
+      <div className="relative isolate px-6 pt-14 lg:px-8">
+        <div
+          aria-hidden="true"
+          className="overflow-hidden blur-3xl"
+        >
+        </div>
+        <div className="mx-auto max-w-2xl py-8 sm:py-12 lg:py-16">
+          <div className="text-center">
+            <h1 className="text-balance text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl">
+            Track Movements with Precision and Confidence 
+            </h1>
+            <p className="mt-8 text-pretty text-lg font-medium text-gray-500 sm:text-xl/8">
+            Monitor every move with real-time tracking. Stay informed, stay ahead, and ensure you never lose sight of what matters most.
+            </p>
+            <div className="mt-10 flex items-center justify-center gap-x-6">
+              <a
+                href="#"
+                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Get started
+              </a>
+              <a href="#" className="text-sm/6 font-semibold text-gray-900">
+                Learn more <span aria-hidden="true">→</span>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div
+          aria-hidden="true"
+          className="blur-3xl"
+        >
+        </div>
+      </div>
+    </div>
+  )
 }
 
 const Feature = () => {
