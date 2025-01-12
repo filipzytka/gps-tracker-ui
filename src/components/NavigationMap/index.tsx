2cab2041e9bccceecf10ba75daf18f0e8d11885e
Filipus01 <101358254+Filipus01@users.noreply.github.com>
2025-01-12 19:40:47 +0100

    add userQuery

--- src/components/NavigationMap/index.tsx
+++ src/components/NavigationMap/index.tsx
@@ -1,21 +1,32 @@
-'use client'
+"use client";
 
-import { MapContainer, TileLayer } from "react-leaflet"
+import { MapContainer, TileLayer, Polyline } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import { LatLngTuple } from "leaflet";
+import { useQuery } from "@tanstack/react-query";
+import { fetchNavigationData } from "@/api/navigation";
 
 const NavigationMap = () => {
+  const { data: navData, refetch: navDataRefetch } = useQuery({
+    queryKey: ["navigationData"],
+    select: (newData) => ({
+      latLng: newData.map((nd): LatLngTuple => [nd.lat, nd.lon]),
+    }),
+    queryFn: () => fetchNavigationData(1, 20),
+  });
   return (
-      <MapContainer
-        center={[51.505, -0.09]}
-        zoom={13}
-        className="w-full h-full"
-      >
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution="&copy; OpenStreetMap contributors"
-        />
-      </MapContainer>
-  )
-}
+    <MapContainer
+      center={[51.64954, 16.11904]}
+      zoom={13}
+      className="w-full h-full"
+    >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution="&copy; OpenStreetMap contributors"
+      />
+      <Polyline positions={navData?.latLng ?? []}/>
+    </MapContainer>
+  );
+};
 
-export default NavigationMap
+export default NavigationMap;
